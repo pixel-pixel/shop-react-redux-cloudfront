a2889e962d806e61ecb9295bfe2af9b146b4f8d7
Andrii Bartish <Andrei.Bartish@primark.com>
2023-03-08 00:16:27 +0200

    feat: FE done

--- lambda/product-service/src/index.js
+++
@@ -1,7 +0,0 @@
-import { getProductsHandler } from "./handlers/getProducts.handler"
-
-export const mainHandler = (event) => {
-  if (!/.+products$/.test(event.path)) return
-
-  return getProductsHandler()
-}
