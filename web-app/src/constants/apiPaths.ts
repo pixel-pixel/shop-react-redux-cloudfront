651773920da32d47679a57354723b1bb564147ba
Andrii Bartish <Andrei.Bartish@primark.com>
2023-03-25 20:33:45 +0200

    update front-end

--- web-app/src/constants/apiPaths.ts
+++ web-app/src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://h9s3t8sb79.execute-api.eu-west-1.amazonaws.com/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://sd3n59ldi9.execute-api.eu-west-1.amazonaws.com/",
   bff: "https://h9s3t8sb79.execute-api.eu-west-1.amazonaws.com/",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
